409b875198a802021cddf102436e39e0843dd2e7
Kunwar <kunwarabhay05@gmail.com>
2024-08-03 18:40:38 +0530

    modal added from mui

--- pantryapp/app/page.js
+++ pantryapp/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { Typography, Button } from '@mui/material';
+import { Typography, Button , Modal} from '@mui/material';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import { Firestore } from 'firebase/firestore';
@@ -8,6 +8,9 @@ import { useEffect, useState } from 'react';
 import {firestore} from '../firebase';
 export default function Home() {
   const [pantry, setPantry]=useState([])
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   useEffect(() => {
     const updatePantry = async () => {
     const snapshot=query(collection(firestore, 'pantry'))
@@ -29,10 +32,38 @@ export default function Home() {
   justifyContent={'center'}
   alignItems={'center'}
   flexDirection={'column'}
-  
+  gap={2}
   >
+    <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: '400px',
+          height: '200px',
+          bgcolor: 'background.paper',
+          border: '2px solid #000',
+          boxShadow: 24,
+          p: 4,
+        }}
+>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal> 
+    <Button variant="contained" onClick={handleOpen}>Add</Button>
     <Box border={'1px solid #333'}>
-    <Button variant="contained">Contained</Button>
     <Box width="800px" height="100px" bgcolor={'#ADD8E6'} display={'flex'} justifyContent={'center'} alignItems={'center'}>
     <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
       Pantry Items
