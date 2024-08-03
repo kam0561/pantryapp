150606969d00249fc6f25203cbd151646d18d9ef
Kunwar <kunwarabhay05@gmail.com>
2024-08-03 12:50:45 +0530

    typography added

--- pantryapp/app/page.js
+++ pantryapp/app/page.js
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 const item=[
@@ -7,6 +8,8 @@ const item=[
   'garlic',
   'ginger',
   'carrot',
+  'lettuce',
+  'cucumber',
 ]
 export default function Home() {
   return (
@@ -17,18 +20,27 @@ export default function Home() {
   justifyContent={'center'}
   alignItems={'center'}
   >
-    <Stack width="800px" height="600px" spacing={2}>
+    <Box width="800px" height="100px" bgcolor={'#f0f0f0'}>
+    <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
+      Pantry Items
+    </Typography>
+    </Box>
+    <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
       {item.map((i)=>(
         <Box 
         key={i}
         width="100%"
-        height="100px"
+        height="300px"
         display={'flex'}
         justifyContent={'center'}
         alignItems={'center'}
         bgcolor={'#f0f0f0'}
         >
-          {i}
+          <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
+            {
+              i.charAt(0).toUpperCase() + i.slice(1)
+            }
+          </Typography>
         </Box>
       ))}
     </Stack>
